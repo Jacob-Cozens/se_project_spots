0c54f00691d9a44b57a491c1ceed135aa7419271
Jacob_Cozens <jacobcozens@rocketmail.com>
2025-01-16 16:29:06 -0500

    Add disabled button

--- scripts/validation.js
+++ scripts/validation.js
@@ -3,14 +3,14 @@ const showInputError = (formEl, inputEl, errorMsg) => {
   const errorMsgEl = formEl.querySelector("#" + errorMsgID);
   errorMsgEl.textContent = errorMsg;
   inputEl.classList.add("modal__input_state_error");
-}
+};
 
 const hideInputError = (formEl, inputEl) => {
   const errorMsgID = inputEl.id + "-error";
   const errorMsgEl = formEl.querySelector("#" + errorMsgID);
   errorMsgEl.textContent = "";
   inputEl.classList.remove("modal__input_state_error");
-}
+};
 
 const checkInputValidity = (formEl, inputEl) => {
   if (!inputEl.validity.valid) {
@@ -20,15 +20,32 @@ const checkInputValidity = (formEl, inputEl) => {
   }
 };
 
+const hasInvalidInput = (inputList) => {
+  return inputList.some((input) => {
+    return !input.validity.valid;
+  });
+};
+
+const toggleButtonState = (inputList, buttonEl) => {
+  if (hasInvalidInput(inputList)) {
+    buttonEl.disabled = true;
+    buttonEl.classList.add("modal__sub-btn_disabled");
+  }
+  else {
+    buttonEl.disabled = false;
+    buttonEl.classList.remove("modal__sub-btn_disabled");
+  }
+};
+
 const setEventListeners = (formEl) => {
   const inputList = Array.from(formEl.querySelectorAll(".modal__input"));
   const buttonElement = formEl.querySelector(".modal__button");
-  //toggleButtonState(inputList, buttonElement);
+  // toggleButtonState(inputList, buttonElement);
 
   inputList.forEach((inputElement) => {
     inputElement.addEventListener("input", function () {
       checkInputValidity(formEl, inputElement);
-      //toggleButtonState(inputList, buttonElement);
+      //  toggleButtonState(inputList, buttonElement);
     });
   });
 };
