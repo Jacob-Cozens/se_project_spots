727baede8968a8682f89150d5b7a76e48a64e021
Jacob_Cozens <jacobcozens@rocketmail.com>
2025-01-17 10:11:56 -0500

    Make it so that a disabled button cannot be hovered over

--- scripts/validation.js
+++ scripts/validation.js
@@ -38,6 +38,7 @@ const hasInvalidInput = (inputList) => {
 const toggleButtonState = (inputList, buttonElement, config) => {
   if (hasInvalidInput(inputList)) {
     disableButton(buttonElement, config);
+    buttonElement.classList.remove()
   } else {
     buttonElement.disabled = false;
     buttonElement.classList.remove(config.inactiveButtonClass);
