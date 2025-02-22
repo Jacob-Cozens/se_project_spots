bd4da60d88ca002fc92834d2d9982ea0affab689
Jacob_Cozens <jacobcozens@rocketmail.com>
2025-02-22 10:57:08 -0500

    Updates

---
+++ src/pages/index.js
@@ -0,0 +1,182 @@
+import "./index.css";
+import {
+  enableValidation,
+  settings,
+  disableButton,
+  resetValidation,
+} from "../scripts/validation.js";
+
+const initialCards = [
+  {
+    name: "Val Thorens",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg",
+  },
+  {
+    name: "Restaurant terrace",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg",
+  },
+  {
+    name: "An outdoor cafe",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg",
+  },
+  {
+    name: "A very long bridge, over the forest and through the trees",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg",
+  },
+  {
+    name: "Tunnel with morning light",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg",
+  },
+  {
+    name: "Mountain house",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
+  },
+  {
+    name: "Golden Gate bridge",
+    link: "  https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/7-photo-by-griffin-wooldridge-from-pexels.jpg",
+  },
+];
+
+console.log(initialCards);
+
+const profileEditButton = document.querySelector(".profile__edit-btn");
+const cardModalButton = document.querySelector(".profile__add-btn");
+const profileName = document.querySelector(".profile__name");
+const profileDescription = document.querySelector(".profile__description");
+
+const profileModal = document.querySelector("#edit-modal");
+const profileFormElement = profileModal.querySelector(".modal__form");
+const profileCloseButton = profileModal.querySelector(".modal__cls-btn");
+const profileModalNameInput = profileModal.querySelector("#modal-name");
+const profileModalDescriptionInput =
+  profileModal.querySelector("#modal-description");
+
+const cardModal = document.querySelector("#add-card-modal");
+const cardForm = cardModal.querySelector(".modal__form");
+const cardSubmitBtn = cardModal.querySelector(".modal__sub-btn");
+const cardprofileCloseButton = cardModal.querySelector(".modal__cls-btn");
+const cardNameInput = cardModal.querySelector("#add-card-name-input");
+const cardLinkInput = cardModal.querySelector("#add-card-link-input");
+
+const previewModal = document.querySelector("#preview-modal");
+const previewModalImgEl = previewModal.querySelector(".modal__img");
+const previewModalCapEl = previewModal.querySelector(".modal__caption");
+const modalCloseTypePreview = previewModal.querySelector(".modal__cls-btn");
+
+const cardTemplate = document.querySelector("#card-template");
+const cardsList = document.querySelector(".cards__list");
+
+function getCardElement(data) {
+  const cardElement = cardTemplate.content
+    .querySelector(".card")
+    .cloneNode(true);
+
+  const cardNameElement = cardElement.querySelector(".card__title");
+  const cardImageElement = cardElement.querySelector(".card__img");
+  const cardLikeBtn = cardElement.querySelector(".card__like-btn");
+
+  cardNameElement.textContent = data.name;
+  cardImageElement.src = data.link;
+  cardImageElement.alt = data.name;
+
+  const cardDeleteBtn = cardElement.querySelector(".card__delete-btn");
+  cardDeleteBtn.addEventListener("click", () => {
+    const index = initialCards.findIndex(
+      (item) => item.name === data.name && item.link === data.link
+    );
+    if (index !== -1) {
+      initialCards.splice(index, 1);
+    }
+    cardElement.remove();
+  });
+
+  cardLikeBtn.addEventListener("click", () => {
+    cardLikeBtn.classList.toggle("card__like-btn_liked");
+  });
+
+  cardImageElement.addEventListener("click", () => {
+    openModal(previewModal);
+    previewModalImgEl.src = data.link;
+    previewModalCapEl.textContent = data.name;
+    previewModalImgEl.alt = data.name;
+  });
+
+  return cardElement;
+}
+
+function handleEditFormSubmit(evt) {
+  evt.preventDefault();
+  profileName.textContent = profileModalNameInput.value;
+  profileDescription.textContent = profileModalDescriptionInput.value;
+  closeModal(profileModal);
+}
+
+function handleAddCardSubmit(evt) {
+  evt.preventDefault();
+  const inputValues = { name: cardNameInput.value, link: cardLinkInput.value };
+  const cardEl = getCardElement(inputValues);
+  cardsList.prepend(cardEl);
+  evt.target.reset();
+  disableButton(cardSubmitBtn, settings);
+  closeModal(cardModal);
+}
+
+profileEditButton.addEventListener("click", () => {
+  profileModalNameInput.value = profileName.textContent;
+  profileModalDescriptionInput.value = profileDescription.textContent;
+  resetValidation(
+    profileFormElement,
+    [profileModalNameInput, profileModalDescriptionInput],
+    settings
+  );
+  openModal(profileModal);
+});
+
+profileCloseButton.addEventListener("click", () => {
+  closeModal(profileModal);
+});
+
+cardModalButton.addEventListener("click", () => {
+  openModal(cardModal);
+});
+
+cardprofileCloseButton.addEventListener("click", () => {
+  closeModal(cardModal);
+});
+
+modalCloseTypePreview.addEventListener("click", () => closeModal(previewModal));
+
+profileFormElement.addEventListener("submit", handleEditFormSubmit);
+cardForm.addEventListener("submit", handleAddCardSubmit);
+
+initialCards.forEach((item) => {
+  const cardEl = getCardElement(item);
+  cardsList.append(cardEl);
+});
+
+function handleEscape(evt) {
+  if (evt.key === "Escape") {
+    const openModal = document.querySelector(".modal.modal_opened");
+    if (openModal) closeModal(openModal);
+  }
+}
+
+function handleOverlayClick(evt) {
+  if (evt.target === evt.currentTarget) {
+    closeModal(evt.currentTarget);
+  }
+}
+
+function openModal(modal) {
+  modal.classList.add("modal_opened");
+  document.addEventListener("keydown", handleEscape);
+  modal.addEventListener("click", handleOverlayClick);
+}
+
+function closeModal(modal) {
+  modal.classList.remove("modal_opened");
+  document.removeEventListener("keydown", handleEscape);
+  modal.removeEventListener("click", handleOverlayClick);
+}
+
+enableValidation(settings);
